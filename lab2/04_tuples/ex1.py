19aeae06c3e3b10da52a53318cbc8b60b7aeceaa
maksutb15 <maksutb15@gmail.com>
2024-02-01 11:45:00 +0600

    bwwwwwwww

---
+++ lab2/04_tuples/ex1.py
@@ -0,0 +1,4 @@
+#Use the correct syntax to print the first item in the fruits tuple.
+
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
---
+++ lab2/05_sets/ex5.py
@@ -0,0 +1,4 @@
+#Use the discard method to remove "banana" from the fruits set.
+
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
